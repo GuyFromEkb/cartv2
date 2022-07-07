3917af89c8c6eed2ca255f3126f30b3fbdd859dc
Sergey <graygmai1@gmail.com>
2022-07-08 03:38:10 +0500

    осталась корзина

--- src/js/script.js
+++ src/js/script.js
@@ -9,46 +9,110 @@ document.addEventListener('DOMContentLoaded', () => {
         const formDataObj = (Object.fromEntries(formData.entries()));
 
         // console.log(formDataJson)
-        renderCart(formDataObj)
+        renderItem(formDataObj)
+        checkListEmpty();
         e.target.reset();
 
     })
+    //Поиск по id, если в списке уже есть товар с таким id, возвраает данный элемент
+    function searchId(id) {
+        const itemInList = document.querySelector('.list__wrap').querySelector(`[data-id="${id}"]`);
+
+        return (itemInList) ? itemInList.closest('.item') : false;
+
+    }
 
     //Отрисовка элемента в корзине 
-    function renderCart({ name, price, count }) {
 
-        const cartItem =
-            `
+    function renderItem({ name, id, price }) {
+        const item = searchId(id);
+
+        //Меняет имя и цену, елси данный товар присутсвует в корзине
+        if (item) {
+
+            item.querySelector('[data-price]').innerText = price + " Р";
+            item.querySelector('.item__name p').innerText = name;
+        }
+        else {
+            //Либо отрисовывает
+            const listItem =
+                `
                 <div class="item">
                     <div class="item__close"></div>
                     <div class="item__name">
-                        <p>Название товара:</p>
-                        <span>${name}</span>
+                        <p>${name}</p> 
+                        
                     </div>
-                    <div class="item__count">
-                        <p>кол-во товара:</p>
-                        <span>${count}</span>
+                    <div data-id="${id}" class="item__id">
+                        <p>ID: <span>${id}</span></p>
+
                     </div>
                     <div class="item__price">
-                        <p>Цена товара:</p>
-                        <span>${price + " Р"}</span>
+                        <span data-price>${price + " Р"}</span>
+                        <span data-discount=""></span>
+                        
                     </div>
                 </div>
              `;
 
-        document.querySelector('.cart__wrap').insertAdjacentHTML('beforeend', cartItem);
+            document.querySelector('.list__wrap').insertAdjacentHTML('beforeend', listItem);
+
+        }
+
+
+
     }
+
+
     //Удаление элемента из корзины
-    function deleteCart() {
-        document.querySelector('.cart__wrap').addEventListener('click', (e) => {
-            console.log(e.target);
-            const target= e.target;
-            if(target.classList.contains('item__close'))
-            {
+
+    function deleteItem() {
+        document.querySelector('.list__wrap').addEventListener('click', (e) => {
+            // console.log(e.target);
+            const target = e.target;
+            if (target.classList.contains('item__close')) {
                 target.closest('.item').remove();
+                checkListEmpty()
             }
         })
     }
-    deleteCart();
+    deleteItem();
+
+    //Добавление сообщения о том, что корзина пуста
+
+    function checkListEmpty() {
+        const itemsInLists = document.querySelector('.list__wrap').children.length;
+
+        // console.log(itemsInLists)
+
+        if (itemsInLists === 0) {
+            document.querySelector('.list__empty').classList.add('show');
+            document.querySelector('.list__title').classList.remove('show');
+
+        }
+        else {
+            document.querySelector('.list__title').classList.add('show');
+            document.querySelector('.list__empty').classList.remove('show');
+        }
+        calculation()
+
+    }
+    checkListEmpty()
+
+    // подсчёт кол-во и суммы
+    function calculation() {
+        const allItems = Array.from(document.querySelector('.list__wrap').children);
+
+        const result = allItems.reduce((acc, item) => {
+
+            acc += parseInt(item.querySelector('.item__price span').innerText);
+            return acc;
+
+        }, 0)
+
+        document.querySelector('.list__sum').innerText = result + " Р";
+        document.querySelector('.list__count').innerText = allItems.length;
+    }
+
 
 });
