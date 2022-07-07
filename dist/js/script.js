185e8fb99b9602538ba6798a58a4daad4b2cd9cf
Sergey <graygmai1@gmail.com>
2022-07-08 01:27:28 +0500

    удаление + добавление товара в корзину

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,54 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', () => {
 
+    //Данные с формы
+    document.querySelector('.form-item').addEventListener('submit', (e) => {
+        e.preventDefault();
 
+        const formData = new FormData(e.target);
+        const formDataObj = (Object.fromEntries(formData.entries()));
+
+        // console.log(formDataJson)
+        renderCart(formDataObj)
+        e.target.reset();
+
+    })
+
+    //Отрисовка элемента в корзине 
+    function renderCart({ name, price, count }) {
+
+        const cartItem =
+            `
+                <div class="item">
+                    <div class="item__close"></div>
+                    <div class="item__name">
+                        <p>Название товара:</p>
+                        <span>${name}</span>
+                    </div>
+                    <div class="item__count">
+                        <p>кол-во товара:</p>
+                        <span>${count}</span>
+                    </div>
+                    <div class="item__price">
+                        <p>Цена товара:</p>
+                        <span>${price + " Р"}</span>
+                    </div>
+                </div>
+             `;
+
+        document.querySelector('.cart__wrap').insertAdjacentHTML('beforeend', cartItem);
+    }
+    //Удаление элемента из корзины
+    function deleteCart() {
+        document.querySelector('.cart__wrap').addEventListener('click', (e) => {
+            console.log(e.target);
+            const target= e.target;
+            if(target.classList.contains('item__close'))
+            {
+                target.closest('.item').remove();
+            }
+        })
+    }
+    deleteCart();
 
 });
